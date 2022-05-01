27e2eaf1f4e50a4a96a8f7587ef47e4f8b180bc6
Emi-N-M <emilio-0111@hotmail.com>
2022-05-01 15:54:50 +0100

    Movement detected + Alarm scripts added

--- Events/Event_Listener.py
+++ Events/Event_Listener.py
@@ -1,5 +1,6 @@
 from Event import subscribe
 from Camera.Cam_Controller import setStopRecording, exit_cam
+from Timer_Event import stop_timer
 
 
 
@@ -14,3 +15,11 @@ def setup_cam_event_handelers():
     subscribe("end_of_recording", handle_stopRecording_Event)
     subscribe("exit_app", handle_turnOff_camera_Event)
 
+
+#Timer listener
+def handle_stopTimer_Event(data):
+    stop_timer(data)
+
+
+def setup_timer_event_handelers():
+    subscribe("exit_app", stop_timer)
